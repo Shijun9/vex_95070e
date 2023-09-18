7597bc083f06ce1e48d69a0cd4e3d5f4b6e631ff
Mihika Nayyar <mihika.nayyar@gmail.com>
2023-09-18 16:07:32 -0700

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -354,7 +354,7 @@ void usercontrol(void) {
       Catapult.stop();
     }
 
-    if (Controller1.ButtonUp.pressing()){
+    if (Controller1.ButtonDown.pressing()){
       Catapult.spinFor(forward, 360, degrees);
     }
 
