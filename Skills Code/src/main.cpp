f92b13b61b918ff87db3a07b6704c1e2f14802ce
Mihika Nayyar <mihika.nayyar@gmail.com>
2023-09-18 17:17:32 -0700

    Minor change to skills

--- Skills Code/src/main.cpp
+++ Skills Code/src/main.cpp
@@ -128,68 +128,7 @@ void place1(void) {
   }
 }
 
-void moreComplicatedAuton1(void){
-  //score the perload triball
-  //then score the triball right outside the goal
-  Intake.spinFor(reverse, 720, degrees);
-  moveDistance(45);
-  turnDegrees(90);
-  moveDistance(12);
-  Intake.spinFor(forward, 720, degrees);
-  moveDistance(-10);
-  turnDegrees(180);
-  moveDistance(-12);
-  moveDistance(10);
-  turnDegrees(-90);
-  moveDistance(12);
-  turnDegrees(90);
-  moveDistance(15);
-  Intake.spinFor(forward, 720, degrees);
-  turnDegrees(180);
-  moveDistance(30);
-  Intake.spinFor(reverse, 720, degrees);
-  moveDistance(-10);
-  turnDegrees(180);
-  moveDistance(-10);
 
- 
-}
-
-void moreComplicatedAuton2(void){
-  //score the perload triball
-  //then score the triball right outside the goal
-  //scoring the triball that was behind the other triball that we scored
-  Intake.spinFor(reverse, 720, degrees);
-  moveDistance(45);
-  turnDegrees(90);
-  moveDistance(12);
-  Intake.spinFor(forward, 720, degrees);
-  moveDistance(-10);
-  turnDegrees(180);
-  moveDistance(-12);
-  moveDistance(10);
-  turnDegrees(-90);
-  moveDistance(12);
-  turnDegrees(90);
-  moveDistance(15);
-  Intake.spinFor(forward, 720, degrees);
-  turnDegrees(180);
-  moveDistance(30);
-  Intake.spinFor(reverse, 720, degrees);
-  moveDistance(-10);
-  turnDegrees(180);
-  moveDistance(-10);
-  moveDistance(48);
-  Intake.spinFor(forward, 720, degrees);
-  moveDistance(-25);
-  turnDegrees(180);
-  moveDistance(-25);
-  Intake.spinFor(reverse, 720, degrees);
-  moveDistance(-10);
-  turnDegrees(180);
-  moveDistance(-10);
-
-}
 
 /*---------------------------------------------------------------------------*/
 /*                                                                           */
